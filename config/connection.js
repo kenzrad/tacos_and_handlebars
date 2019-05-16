390a9e7294c3afa41949369a28833114e3ee4fe8
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-05-16 18:15:58 -0400

    jawsdb troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var connection;
 
-console.log(`I'm trying ${process.env.JAWSDB_URL}`);
+console.log(`HEEEEEEEEEEEEEEEEEEEYYYYYYYYYYYYYYYYYYYYYYYYY I'm trying ${process.env.JAWSDB_URL}`);
 if(process.env.JAWSDB_URL) {
   console.log("hey I'm in jaws");
   connection = mysql.createConnection(process.env.JAWSDB_URL);
