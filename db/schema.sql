da0fc82291061419c9fe83bf8c01238037eb76dc
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-05-16 18:03:24 -0400

    heroku test

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,26 @@
 ### Schema
 
+##localhost
 CREATE DATABASE taco_db;
 USE taco_db;
 
 CREATE TABLE tacos
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+
+##JAWS DB
+USE `dth635cpfz5ykc8k`;
+
+CREATE TABLE tacos
+(
+	id INT AUTO_INCREMENT NOT NULL,
+	name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
